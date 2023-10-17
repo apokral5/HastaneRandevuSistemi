08d9e5bc2b6f07dd8dac9e9af3b105e9593ce4e6
Abdullah Haktan <haktanabdullah123@gmail.com>
2023-10-17 14:42:58 +0300

    Add project files.

---
+++ HastaneRandevuSistemi/Controllers/HastaneRandevuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HastaneRandevuSistemi.Models.Entity;
+namespace HastaneRandevuSistemi.Controllers
+{
+    public class HastaneRandevuController : Controller
+    {
+        // GET: HastaneRandevu
+        HastaneOtomasyonSistemiEntities1 db =new HastaneOtomasyonSistemiEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.TBL_HASTA.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ HastaneRandevuSistemi/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HastaneRandevuSistemi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Senin hakkında sayfan";
+            return View();
+        }
+        public ActionResult Contact()
+        { 
+            ViewBag.Message = "Senin iletişim sayfan";
+            return View();
+        }
+        public ActionResult Bilgi()
+        {
+            return View();
+        }
+        public ActionResult Profil()
+        {
+          return View();
+        }
+    }
+}
